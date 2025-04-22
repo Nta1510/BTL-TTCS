043fbdce5db1fca779400f04e672b83089cf5dcf
Nta1510 <125479251+Nta1510@users.noreply.github.com>
2025-04-23 05:51:58 +0700

    Update urls.py

--- Blog-web/ myblog_project/urls.py
+++ Blog-web/ myblog_project/urls.py
@@ -1,1 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),  # URL của app accounts
+    path('messages/', include('messaging.urls')), # URL của app messaging
+    path('', include('blog.urls')),  # URL của app blog (trang chủ)
+]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
