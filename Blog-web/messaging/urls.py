0d0e1107afd88321df7efd883fd54b6e9f248afe
Nta1510 <125479251+Nta1510@users.noreply.github.com>
2025-04-23 06:01:18 +0700

    Update urls.py

--- Blog-web/messaging/urls.py
+++ Blog-web/messaging/urls.py
@@ -1,1 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.inbox, name='inbox'),
+    path('sent/', views.sent_messages, name='sent_messages'),
+    path('send/', views.send_message, name='send_message'),
+    path('message/<int:pk>/', views.message_detail, name='message_detail'),
+]
 
