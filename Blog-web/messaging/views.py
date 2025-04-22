a2477166ba39913c2703f2995d2d36ea0c093ed1
Nta1510 <125479251+Nta1510@users.noreply.github.com>
2025-04-23 06:01:41 +0700

    Update views.py

--- Blog-web/messaging/views.py
+++ Blog-web/messaging/views.py
@@ -1,1 +1,35 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Message
+from .forms import MessageForm
 
+@login_required
+def inbox(request):
+    messages = Message.objects.filter(recipient=request.user).order_by('-timestamp')
+    return render(request, 'messaging/inbox.html', {'messages': messages})
+
+@login_required
+def sent_messages(request):
+    messages = Message.objects.filter(sender=request.user).order_by('-timestamp')
+    return render(request, 'messaging/sent.html', {'messages': messages})
+
+@login_required
+def message_detail(request, pk):
+    message = get_object_or_404(Message, pk=pk)
+    if message.recipient == request.user and not message.is_read:
+        message.is_read = True
+        message.save()
+    return render(request, 'messaging/message_detail.html', {'message': message})
+
+@login_required
+def send_message(request):
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            msg = form.save(commit=False)
+            msg.sender = request.user
+            msg.save()
+            return redirect('inbox')
+    else:
+        form = MessageForm()
+    return render(request, 'messaging/send_message.html', {'form': form})
